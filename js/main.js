0a9519943e6612a05a60fcaedb80fecc1d2ce5aa
adequateremedy <157491572+adequateremedy@users.noreply.github.com>
2026-07-06 19:38:49 -0500

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-fetch('data/chapters.json')
-  .then(response => response.json())
-  .then(data => {
-    console.log("CHAPTER DATA LOADED:");
-    console.log(data);
-  })
-  .catch(error => {
-    console.error("ERROR LOADING CHAPTER DATA:", error);
-  });
